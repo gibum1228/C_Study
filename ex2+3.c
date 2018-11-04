ba8593872616045ab460831bdb37beb5b75de513
gibum1228 <gibum1228@naver.com>
2018-11-04 14:25:37 +0900

    두 수의 합 구하기
    2018.11.04.

---
+++ ex2+3.c
@@ -0,0 +1,16 @@
+// plus program
+#include<stdio.h>
+
+int main()
+{
+	int x, y;
+
+	printf("숫자 입력:");
+	scanf("%d", &x);
+	printf("숫자 입력:");
+	scanf("%d", &y);
+
+	printf("두 수의 합:%d\n", x + y);
+
+	return 0;
+}
