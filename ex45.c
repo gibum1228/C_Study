68c880cfbd4e8eca09fef49eba16449061649eb6
gibum1228 <gibum1228@naver.com>
2018-11-12 22:01:17 +0900

    2018.11.12.
    함수를 이용해 소수 판단하는 프로그램

---
+++ ex45.c
@@ -0,0 +1,31 @@
+/*201814066 김기범
+함수를 이용해 소수 판단 프로그램*/
+#include<stdio.h>
+
+int main()
+{
+	int is_prime(unsigned n);
+	unsigned n;
+
+	printf("정수를 입력하시오:");
+	scanf("%d", &n);
+
+	if (is_prime(n))
+		printf("%d은 소수입니다.\n", n);
+	else
+		printf("%d은 소수가 아닙니다.\n", n);
+
+	return 0;
+}
+
+int is_prime(unsigned n) {
+	int i, n2;
+
+	if (n < 2) return 0;
+	
+	for (n2 = n / 2, i = 2; i <= n2; i++)
+		if (n % i == 0)
+			return 0;
+
+	return 1;
+}
