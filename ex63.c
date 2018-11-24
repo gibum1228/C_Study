83e31bd2d20f0e4cb09cbf55ec2e7eb5d0b3d860
gibum1228 <gibum1228@naver.com>
2018-11-24 13:33:52 +0900

    2018.11.24.
    막대 그래프 만드는 프로그램

---
+++ ex63.c
@@ -0,0 +1,20 @@
+/*201814066 김기범
+막대 그래프 만드는 프로그램*/
+#include<stdio.h>
+#define STUDENTS 5
+
+int main()
+{
+	int grade[STUDENTS] = { 30, 20, 10, 40, 50 };
+	int i, s;
+
+	for (i = 0; i < STUDENTS; i++)
+	{
+		printf("번호 %d: ", i);
+		for (s = 0 ; s < grade[i]; s++)
+			printf("*");
+		printf("\n");
+	}
+
+	return 0;
+}
