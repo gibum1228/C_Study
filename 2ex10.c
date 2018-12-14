43f27ffb65f1c76465a846aa7aa1902e815be7e0
gibum1228 <gibum1228@naver.com>
2018-12-14 22:48:44 +0900

    2018.12.14.
    함수형 매크로
    SQUARE

---
+++ 2ex10.c
@@ -0,0 +1,17 @@
+// 함수형 매크로(SQUARE)
+#include <stdio.h>
+
+#define SQUARE(x)  ((x) * (x))
+
+int main(void) {
+	int x = 2;
+
+	printf("%d\n", SQUARE(x));
+	printf("%d\n", SQUARE(3));
+	printf("%f\n", SQUARE(1.2));
+	printf("%d\n", SQUARE(x + 3));
+	printf("%d\n", 100 / SQUARE(x));
+	printf("%d\n", SQUARE(++x));	// 논리 오류
+
+	return 0;
+}
